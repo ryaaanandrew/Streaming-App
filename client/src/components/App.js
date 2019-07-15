861c8702771b4ba4bc13178d9c48e7d973f62fec
Ryan Andrew Sibat <ryanandrew.sibat@gmail.com>
2019-07-15 16:33:27 -0400

    created crud components and routes via react-router-dom

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+import { StreamCreate, StreamDelete, StreamEdit, StreamShow, StreamList } from './streams';
 
 const App = () => {
-    return <div>App</div>
+    return (
+        <BrowserRouter>
+            <div>
+                <Route path='/' exact component={StreamList} />
+                <Route path='/streams/new' exact component={StreamCreate} />
+                <Route path='/streams/edit' exact component={StreamEdit} />
+                <Route path='/streams/delete' exact component={StreamDelete} />
+                <Route path='/streams/show' exact component={StreamShow} />
+            </div>
+        </BrowserRouter>
+    )
 };
 
 export default App;
