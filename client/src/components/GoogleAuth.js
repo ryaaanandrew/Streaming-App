3da1c7037c3a2d7d74cf48b0ddbd32e349fa940c
Ryan Andrew Sibat <ryanandrew.sibat@gmail.com>
2019-07-15 20:59:24 -0400

    basic set up for redux store

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -11,17 +11,40 @@ class GoogleAuth extends Component {
             }).then(() => {
                 this.auth = window.gapi.auth2.getAuthInstance();
                 this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+                this.auth.isSignedIn.listen(this.onAuthChange)
             })
         });
     };
 
+    onAuthChange = () => {
+        this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+    }
+
+    onSignInClick = () => {
+        this.auth.signIn();
+    }
+
+    onSignOutClick = () => {
+        this.auth.signOut();
+    }
+
     renderAuthButton = () => {
         if (this.state.isSignedIn === null) {
-            return <div>I dont know if user is logged in </div>
+            return null
         } else if ( this.state.isSignedIn ) {
-            return <div> I am signed in </div>
+            return (
+                <button className='ui red google button' onClick={this.onSignOutClick}>
+                    <i className='google icon'/>
+                    Sign out
+                </button>
+            )
         } else {
-            return <div> I am not signed in </div>
+            return (
+                <button className='ui red google button' onClick={this.onSignInClick}>
+                    <i className='google icon'/>
+                    Sign in with Google
+                </button>
+            )
         };
     } 
 
