0cde8dfa95febdafe50573139af71eb7f9a001ef
Ryan Andrew Sibat <ryanandrew.sibat@gmail.com>
2019-07-19 17:21:50 -0400

    set up streaming via obs

--- client/src/components/streams/StreamShow.jsx
+++ client/src/components/streams/StreamShow.jsx
@@ -11,9 +11,31 @@ class StreamShow extends Component {
     }
 
     componentDidMount() {
-        this.props.fetchStream(this.props.match.params.id);
+        const { id } = this.props.match.params;
+        
+        this.props.fetchStream(id);
+        this.buildPlayer();
     };
-    
+
+    componentDidUpdate() {
+        this.buildPlayer();
+    }
+
+    buildPlayer = () => {
+        const { id } = this.props.match.params;
+
+        if (this.player || !this.props.stream) {
+            return;
+        }
+
+        this.player = flv.createPlayer({
+            type: 'flv',
+            url: `http://localhost:8000/live/${id}.flv`
+        });
+        this.player.attachMediaElement(this.videoRef.current);
+        this.player.load();  
+    };
+
     render() {
         if(!this.props.stream) {
             return <div>Loading...</div>
